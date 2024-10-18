bdbb894178a59712cec2315bbced5f09dab9d101
Shivanessh <shivanesshrs00@gmail.com>
2024-10-18 18:41:54 +0530

    Update SearchinLinkedList.cpp

--- c++/SearchinLinkedList.cpp
+++ c++/SearchinLinkedList.cpp
@@ -1,70 +1,75 @@
 #include <iostream>
 using namespace std;
-class Node
-{
-    public:
+
+class Node {
+public:
     int data;
-    Node *next;
-}
-*first;
-void Create(int A[],int n)
-{
-    Node *t,*last;
-    int i;
-    first=new Node;
-    first->data=A[0];
-    first->next=NULL;
-    last=first;
-    for(i=1;i<n;i++)
-    {
-        t=new Node;
-        t->data=A[i];
-        t->next=NULL;
-        last->next=t;
-        last=t;
+    Node* next;
+};
+
+// Global pointer to the first node of the linked list
+Node* first = nullptr;
+
+void Create(int A[], int n) {
+    Node* t, * last;
+    first = new Node; // Create the first node
+    first->data = A[0];
+    first->next = nullptr;
+    last = first; // Set last to the first node
+
+    // Create the rest of the nodes
+    for (int i = 1; i < n; i++) {
+        t = new Node; // Create a new node
+        t->data = A[i];
+        t->next = nullptr;
+        last->next = t; // Link the last node to the new node
+        last = t; // Update last to the new node
     }
 }
-Node* Search(Node *head,int key)
-{
-    Node *p=head;
-    while(p!=NULL)
-    {
-        if(p->data==key)
-        {
-            cout<<"\nElement found";
+
+Node* Search(Node* head, int key) {
+    Node* p = head;
+    while (p != nullptr) {
+        if (p->data == key) {
+            cout << "\nElement found";
             return p;
         }
-        p=p->next;
+        p = p->next;
     }
+    cout << "\nElement not found";
+    return nullptr;
 }
-Node* RecSearch(Node *head,int key)
-{
-    Node *p=head;
-    if(p==NULL)
-    return NULL;
-    if(p->data==key)
-    return p;
-    else
-    return RecSearch(p->next,key);
+
+Node* RecSearch(Node* head, int key) {
+    if (head == nullptr)
+        return nullptr; // Base case: key not found
+    if (head->data == key)
+        return head; // Key found
+    return RecSearch(head->next, key); // Recursive call
 }
-int main()
-{
-    int n,i,a[100];
-    cout<<"Enter the size of the array : ";
-    cin>>n;
-    cout<<"Enter the elements of the array : ";
-    for(i=0;i<n;i++)
-    {
-        cin>>a[i];
+
+int main() {
+    int n, a[100];
+    cout << "Enter the size of the array: ";
+    cin >> n;
+
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
     }
-    Create(a,n);
+
+    Create(a, n); // Create the linked list
+
     int key;
-    cout<<"\nEnter the element to be searched : ";
-    cin>>key;
-    Node *p=RecSearch(first,key);
-    if(p==NULL)
-    cout<<"\nElement not found";
+    cout << "\nEnter the element to be searched: ";
+    cin >> key;
+
+    // Search using recursion
+    Node* p = RecSearch(first, key);
+    if (p == nullptr)
+        cout << "\nElement not found";
     else
-    cout<<"\nElement found  ";
-return 0;
+        cout << "\nElement found: " << p->data;
+
+    return 0;
 }
