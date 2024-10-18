106e2c3dee55490a1d9c45de5ab5d6299a5e872b
Shivanessh <shivanesshrs00@gmail.com>
2024-10-18 19:04:48 +0530

    Update Bubble_Sort.java

--- java/Bubble_Sort.java
+++ java/Bubble_Sort.java
@@ -1,8 +1,9 @@
-public class Bubble_Sort {
+public class BubbleSort {
     public int[] sortArray(int[] nums) {
         for (int i = 0; i < nums.length - 1; i++) {
             for (int j = 0; j < nums.length - i - 1; j++) {
                 if (nums[j] > nums[j + 1]) {
+                    // Swap nums[j] and nums[j + 1]
                     int temp = nums[j];
                     nums[j] = nums[j + 1];
                     nums[j + 1] = temp;
@@ -10,5 +11,16 @@ public class Bubble_Sort {
             }
         }
         return nums;
+    }
+
+    public static void main(String[] args) {
+        BubbleSort sorter = new BubbleSort();
+        int[] nums = {5, 3, 8, 4, 2};
+        int[] sortedArray = sorter.sortArray(nums);
+
+        System.out.print("Sorted array: ");
+        for (int num : sortedArray) {
+            System.out.print(num + " ");
+        }
     }
 }
